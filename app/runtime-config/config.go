4d206b7d7520b54e188030d4c63995c85ea84760
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-11-10 04:02:05 +0100

    refactor: rename runtime config file

--- app/runtime-config/config.go
+++ app/runtime-config/config.go
@@ -1,10 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
 type GatekeeperServerConfig struct {
-	ServerPort int `split_words:"true" default:"3333"`
+	GatekeeperServerEnabled bool `split_words:"true" default:"true"`
+	GatekeeperServerPort    int  `split_words:"true" default:"8081"`
 }
 
 type LivenessProxyConfig struct {
+	LivenessTargetEnabled  bool   `split_words:"true" default: "false"`
 	LivenessTargetProtocol string `split_words:"true" default:"http"`
 	LivenessTargetHostname string `split_words:"true" default:"127.0.0.1"`
 	LivenessTargetPort     int    `split_words:"true" default:"8080"`
@@ -12,6 +20,7 @@ type LivenessProxyConfig struct {
 }
 
 type HealthProxyConfig struct {
+	HealthTargetEnabled  bool   `split_words:"true" default: "false"`
 	HealthTargetProtocol string `split_words:"true" default:"http"`
 	HealthTargetHostname string `split_words:"true" default:"127.0.0.1"`
 	HealthTargetPort     int    `split_words:"true" default:"8080"`
@@ -22,4 +31,28 @@ type Config struct {
 	HealthProxyConfig
 	LivenessProxyConfig
 	GatekeeperServerConfig
+}
+
+var c *Config
+
+func Load() {
+	var s Config
+	err := envconfig.Process("GATEKEEPER", &s)
+	if err != nil {
+		panic(fmt.Sprintf("unable to read environment configuration %s", err.Error()))
+	}
+
+	c = &s
+}
+
+func Get() Config {
+	if c == nil {
+		Load()
+	}
+
+	return *c
+}
+
+func reset() {
+	c = nil
 }
