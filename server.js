4a65f7b5d4d86fab7d8be1ea4cd053762981ae6e
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-11-17 12:21:39 +0100

    chore: improve signal handling to enure server terminates as expected

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const http = require('http');
-const os = require('os')
-const fs = require('fs')
+const os = require('os');
 
-console.log('user', os.userInfo({ encoding: 'utf8' }))
+// Get OS-specific information (optional)
+const user = os.userInfo({ encoding: 'utf8' });
+console.log({ user })
 
 const port = process.env.PORT || 8080;
-const hostname = '0.0.0.0';
+const hostname = '0.0.0.0'; // Use for local server on the machine
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
+});
+
+// Example of signal handling using "process" API:
+process.on('SIGINT', () => {
+    console.log('Received SIGINT signal, closing server.');
+    server.close(() => {
+        console.log('Server closed successfully!');
+        process.exit(0); // Exit the process after successful closure
+    });
+});
+
+// Additional Error Handling (for graceful shutdown)
+process.once('SIGTERM', () => {
+    console.log("Received SIGTERM signal, shutting down server gracefully.");
+    server.close(() => {
+        console.log("Server closed successfully!");
+        process.exit(0); // Exit the process after successful closure
+    });
 });
 
 server.listen(port, hostname, () => {
