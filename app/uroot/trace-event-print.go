477e6f1be799377b6936f51eb4e3163d5efbb6de
Stefan Pfaffel <s.pfaffel@gmail.com>
2026-01-05 10:48:56 +0100

    chore: update event trace printer
    Signed-off-by: Stefan Pfaffel <s.pfaffel@gmail.com>

--- app/uroot/trace-event-print.go
+++ app/uroot/trace-event-print.go
@@ -13,6 +13,7 @@ package uroot
 import (
 	"fmt"
 	"io"
+	"os"
 
 	// "github.com/cuandari/lib/app/uroot/syscalls/args"
 	"github.com/cuandari/lib/app/uroot/syscalls/args"
@@ -49,7 +50,128 @@ func PrintTraces(w io.Writer) EventCallback {
 
 // SysCallEnter is called each time a system call enter event happens.
 func SysCallEnter(t Task, s *SyscallEvent) string {
+	pid := t.(*process).pid
 	name, _ := sec.ScmpSyscall(s.Regs.Orig_rax).GetName()
+	if name == "access" {
+		pathAddr := s.Args[0].Pointer()
+		var path string
+		var b [1]byte
+		for len(path) < 1024 {
+			if _, err := t.Read(pathAddr, b[:]); err != nil {
+				break
+			}
+			if b[0] == 0 {
+				break
+			}
+			path = path + string(b[:])
+			pathAddr++
+		}
+
+		return fmt.Sprintf("enter %s %s (%s)", t.Name(), name, path)
+
+	} else if name == "fstat" || name == "close" || name == "read" || name == "ioctl" || name == "pread64" || name == "getdents" || name == "getdents64" {
+		dirfd := s.Args[0].Int()
+		var fdPath string
+		fdPath = fmt.Sprintf("/proc/%d/fd/%d", t.(*process).pid, dirfd)
+
+		// Get path from /proc
+		if dirfd == unix.AT_FDCWD {
+			fdPath, _ = os.Getwd()
+		} else {
+			pid := t.(*process).pid
+			if args.IsSocket(pid, dirfd) {
+				println("Closing network socket with fd %d", dirfd)
+				fdPath = "socket"
+			} else if args.IsFile(pid, dirfd) || args.IsDir(pid, dirfd) {
+				if resolvedPath, err := os.Readlink(fdPath); err != nil {
+					fdPath = resolvedPath
+				}
+			}
+		}
+
+		return fmt.Sprintf("enter %s %s (%s)", t.Name(), name, fdPath)
+
+	} else if name == "openat" {
+		dirfd := s.Args[0].Int()
+		pathAddr := s.Args[1].Pointer()
+		flags := s.Args[2].Uint() // or Int() depending on your usage
+		mode := s.Args[3].ModeT() // Assuming mode_t is represented as uint
+
+		var path string
+		var b [1]byte
+		for len(path) < 1024 {
+			if _, err := t.Read(pathAddr, b[:]); err != nil {
+				break
+			}
+			if b[0] == 0 {
+				break
+			}
+			path = path + string(b[:])
+			pathAddr++
+		}
+
+		accessMode := ""
+		if flags&unix.O_RDONLY == unix.O_RDONLY {
+			accessMode = "read"
+		}
+		if flags&unix.O_WRONLY == unix.O_WRONLY {
+			accessMode = "write"
+		}
+		if flags&unix.O_RDWR == unix.O_RDWR {
+			accessMode = "write"
+		}
+		if flags&unix.O_RDWR == unix.O_APPEND {
+			accessMode = "write"
+		}
+		if flags&unix.O_RDWR == unix.O_CREAT {
+			accessMode = "write"
+		}
+		if flags&unix.O_RDWR == unix.O_TRUNC {
+			accessMode = "write"
+		}
+
+		fdType := "fd"
+
+		// Get path from /proc
+		var fdPath string
+		if dirfd == unix.AT_FDCWD {
+			fdPath, _ = os.Getwd()
+		} else {
+			fdPath = fmt.Sprintf("/proc/%d/fd/%d", t.(*process).pid, dirfd)
+			if resolvedPath, err := os.Readlink(fdPath); err == nil {
+				fdPath = resolvedPath
+			} else {
+				println(fmt.Printf("error reading fd path: %v\n", err))
+			}
+		}
+
+		return fmt.Sprintf("enter %s %s(%d:%s,%s, %q, %s, %#o)", t.Name(), name, dirfd, fdType, fdPath, path, accessMode, mode)
+	} else if name == "write" {
+		dirfd := s.Args[0].Int()
+
+		fdType := "fd"
+		if args.IsStdIn(dirfd) {
+			fdType = " (stdin)"
+		} else if args.IsStdOut(dirfd) {
+			fdType = " (stdout)"
+		} else if args.IsStdErr(dirfd) {
+			fdType = " (stderr)"
+		} else if args.IsFile(pid, dirfd) {
+			fdType = " (file)"
+		} else if args.IsDir(pid, dirfd) {
+			fdType = " (directory)"
+		} else if args.IsSocket(pid, dirfd) {
+			fdType = " (socket)"
+		} else if args.IsPipe(pid, dirfd) {
+			fdType = " (pipe)"
+		} else if args.IsBlockDevice(pid, dirfd) {
+			fdType = " (block)"
+		} else if args.IsCharDevice(pid, dirfd) {
+			fdType = " (char)"
+		}
+
+		return fmt.Sprintf("enter %s %s(%d, %s)", t.Name(), name, dirfd, fdType)
+	}
 	return fmt.Sprintf("enter %s %s", t.Name(), name)
 }
 
