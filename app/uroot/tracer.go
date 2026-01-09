26ca879c6c7f43e52c0988b3f916bc2436629132
Stefan Pfaffel <s.pfaffel@gmail.com>
2026-01-09 21:06:51 +0100

    refactor: move trace ent type to its own file
    Signed-off-by: Stefan Pfaffel <s.pfaffel@gmail.com>

--- app/uroot/tracer.go
+++ app/uroot/tracer.go
@@ -23,43 +23,6 @@ func (e *ExitEventError) Error() string {
 	return fmt.Sprintf("Exited with status %d and signal %s", e.ExitCode, e.Signal)
 }
 
-// EventType describes a process event.
-type EventType int
-
-const (
-	// Unknown is for events we do not know how to interpret.
-	Unknown EventType = 0x0
-
-	// SyscallEnter is the event for a process calling a syscall.  Event
-	// Args will contain the arguments sent by the userspace process.
-	//
-	// ptrace calls this a syscall-enter-stop.
-	SyscallEnter EventType = 0x2
-
-	// SyscallExit is the event for the kernel returning a syscall. Args
-	// will contain the arguments as returned by the kernel.
-	//
-	// ptrace calls this a syscall-exit-stop.
-	SyscallExit EventType = 0x3
-
-	// SignalExit means the process has been terminated by a signal.
-	SignalExit EventType = 0x4
-
-	// Exit means the process has exited with an exit code.
-	Exit EventType = 0x5
-
-	// SignalStop means the process was stopped by a signal.
-	//
-	// ptrace calls this a signal-delivery-stop.
-	SignalStop EventType = 0x6
-
-	// NewChild means the process created a new child thread or child
-	// process via fork, clone, or vfork.
-	//
-	// ptrace calls this a PTRACE_EVENT_(FORK|CLONE|VFORK).
-	NewChild EventType = 0x7
-)
-
 type tracer struct {
 	processes map[int]*process
 	callback  []EventCallback
@@ -245,12 +208,12 @@ func (t *tracer) runLoop(cancelFunc context.CancelCauseFunc) {
 						runtime.Get().FileSystemAllowRead &&
 						!runtime.Get().FileSystemAllowWrite:
 						// Gate file open syscalls when only read access is allowed.
-							allow = syscalls.IsOpenAtReadOnly(s, rec.Event == SyscallEnter)
+						allow = syscalls.IsOpenAtReadOnly(s, rec.Event == SyscallEnter)
 					case name == "open" &&
 						runtime.Get().FileSystemAllowRead &&
 						!runtime.Get().FileSystemAllowWrite:
 						// Gate file open syscalls when only read access is allowed.
-							allow = syscalls.IsOpenReadOnly(s, rec.Event == SyscallEnter)
+						allow = syscalls.IsOpenReadOnly(s, rec.Event == SyscallEnter)
 					case name == "write" || name == "writev" || name == "send" || name == "sendmsg" || name == "sendmmsg" || name == "sendto":
 						syscallArgs := rec.Syscall.Args
 						fd := syscallArgs[0].Int()
